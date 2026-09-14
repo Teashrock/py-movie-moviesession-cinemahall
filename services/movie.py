18076e796063aa6da8d27e8568319f3e7ab3d4cc
Teashrock <kajitsu22@gmail.com>
2026-09-14 21:17:41 +0300

    Next

--- services/movie.py
+++ services/movie.py
@@ -5,3 +5,22 @@ def get_movies(genres_ids: list=[], actors_ids: list=[]) -> list:
     movies = Movie.query.all()
     if not genres_ids and not actors_ids:
         return movies
+    if genres_ids and actors_ids:
+        return movies.query.filter(genres_ids).filter(actors_ids).all()
+    if genres_ids:
+        return movies.query.filter(genres_ids).all()
+    if actors_ids:
+        return movies.query.filter(actors_ids).all()
+
+
+def get_movie_by_id(movie_id: int) -> Movie:
+    return Movie.query.all()[movie_id]
+
+
+def create_movie(movie_title: str, movie_description: str, genre_ids: list=[], actors_ids: list=[]) -> None:
+    Movie(
+        title=movie_title,
+        description=movie_description,
+        genres=genre_ids,
+        actors=actors_ids
+    )
---
+++ services/movie_session.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from db.models import MovieSession, CinemaHall, Movie
+
+
+def create_movie_session(movie_show_time: datetime, movie_id: int, cinema_hall_id: int) -> None:
+    MovieSession(
+        show_time=movie_show_time,
+        cinema_hall=CinemaHall.objects.get(id=cinema_hall_id),
+        movie_id=Movie.objects.get(id=movie_id)
+    )
+
+
+def get_movies_session(session_date: str="") -> list[MovieSession]:
+    if session_date:
+        return MovieSession.objects.filter(show_time=datetime.strptime(session_date, '%Y-%m-%d'))
+    return MovieSession.objects.all()
+
+
+def get_movie_session_by_id(movie_id: int) -> MovieSession:
+    return MovieSession.objects.get(id=movie_id)
+
+
+def update_movie_session(session_id: int, show_time: datetime=None, movie_id: int=None, cinema_hall_id: int=None) -> None:
+    MovieSession.objects.filter(id=session_id).update()
