60db81c9a6a8d62af536d29ffacce9a55367b051
Teashrock <kajitsu22@gmail.com>
2026-09-14 23:30:58 +0300

    Next

--- services/movie.py
+++ services/movie.py
@@ -1,19 +1,21 @@
+from django.db.models import QuerySet
+
 from db.models import Movie
 
 
 def get_movies(
         genres_ids: list = [],
         actors_ids: list = []
-) -> list:
+) -> list[Movie]:
     movies = Movie.objects.all()
     if not genres_ids and not actors_ids:
         return movies
     if genres_ids and actors_ids:
-        return movies.query.filter(genres_ids).filter(actors_ids).all()
+        return movies.filter(genres_ids).filter(actors_ids).all()
     if genres_ids:
-        return movies.query.filter(genres_ids).all()
+        return movies.filter(genres_ids).all()
     if actors_ids:
-        return movies.query.filter(actors_ids).all()
+        return movies.filter(actors_ids).all()
 
 
 def get_movie_by_id(movie_id: int) -> Movie:
